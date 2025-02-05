091bc59c1b3500db24aa315d73469fcc3a21c916
Илья <142158071+lyasotailya@users.noreply.github.com>
2025-02-05 20:12:28 +0300

    2

--- src/index.js
+++ src/index.js
@@ -5,33 +5,26 @@ import "./js/app";
 // TODO: write your code in app.js
 
 function getRandomCell() {
-    const cells = document.querySelectorAll('.cell');
-    const randomIndex = Math.floor(Math.random() * cells.length);
-    return cells[randomIndex];
+  const cells = document.querySelectorAll(".cell");
+  const randomIndex = Math.floor(Math.random() * cells.length);
+  return cells[randomIndex];
 }
 
 function moveImage() {
-    const cells = document.querySelectorAll('.cell');
-    cells.forEach(cell => {
-        const img = cell.querySelector('img');
-        if (img) {
-            img.style.display = 'none';
-        }
-    });
-
-    const randomCell = getRandomCell();
-    let img = randomCell.querySelector('img');
-
-    if (!img) {
-        img = document.createElement('img');
-        img.src = 'img/goblin.png';
-        randomCell.appendChild(img);
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
+    const img = cell.querySelector("img");
+    if (img) {
+      img.style.display = "none";
     }
+  });
 
-    img.style.display = 'block';
+  const randomCell = getRandomCell();
+  let img = randomCell.querySelector("img");
+  img.style.display = "block";
 }
 
 window.onload = () => {
-    moveImage();
-    setInterval(moveImage, 1000);
+  moveImage();
+  setInterval(moveImage, 1000);
 };

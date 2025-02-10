e086e33e8799086d5377376d33d50282b7d38d69
Irina <i.liasota@ya.ru>
2025-02-10 21:13:58 +0300

    modification 1

--- src/index.js
+++ src/index.js
@@ -1,27 +1,44 @@
 import "./css/style.css";
-
+import img_goblin from "./img/goblin.png";
 import "./js/app";
 
 // TODO: write your code in app.js
 
-function getRandomCell() {
+function getRandomInt() {
   const cells = document.querySelectorAll(".cell");
   const randomIndex = Math.floor(Math.random() * cells.length);
-  return cells[randomIndex];
+  return randomIndex;
 }
 
 function moveImage() {
-  const cells = document.querySelectorAll(".cell");
-  cells.forEach((cell) => {
-    const img = cell.querySelector("img");
-    if (img) {
-      img.style.display = "none";
-    }
-  });
+  const cells = document.querySelectorAll('.cell');
+    let currentCellWithImage = null;
+    cells.forEach((cell, index) => {
+      const img = cell.querySelector('img');
+      if (img && img.style.display !== 'none') {
+        currentCellWithImage = index;
+      }
+    });
 
-  const randomCell = getRandomCell();
-  let img = randomCell.querySelector("img");
-  img.style.display = "block";
+    if (currentCellWithImage !== null) {
+      const currentImg = cells[currentCellWithImage].querySelector('img');
+      currentImg.style.display = 'none';
+    }
+
+    let randomIndex;
+    do {
+        randomIndex = getRandomInt();
+    } while (randomIndex === currentCellWithImage);
+
+    const newImg = cells[randomIndex].querySelector('img');
+    if (newImg) {
+      newImg.style.display = 'block';
+    } else {
+      const img = document.createElement('img');
+      img.src = img_goblin;
+      img.style.display = 'block';
+      cells[randomIndex].appendChild(img);
+    }
 }
 
 window.onload = () => {
